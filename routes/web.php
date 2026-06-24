5e8c64d3ad650381a4ea3d1ad8a24072de0f030a
Md. Mahbubur Rahman <mahbubur@softvence.com>
2026-06-24 15:19:33 +0600

    content issue

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,17 @@ use App\Http\Controllers\BiomarkerReportController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('user.home');
+// Route::get('/', function () {
+//     return view('user.home');
     
-});
+// });
+
+
+Route::get('/', [UserController::class, 'userHome'])->name('home.index');
+
 Route::prefix('user')->name('user.')->middleware(['auth'])->group(function () {
     
-    Route::get('/dashboard', [UserController::class, 'userHome'])->name('dashboard.index');
+    Route::get('/dashboard', [UserController::class, 'userDashboard'])->name('dashboard.index');
     Route::get('/reports', [BiomarkerReportController::class, 'index'])->name('reports.index');
     
     Route::get('/reports/{id}', [BiomarkerReportController::class, 'showUserReport'])->name('reports.show');
